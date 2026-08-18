385157c4208130ee0c876c6c9fa8bfd774e97181
rodrigoadomes-star <rodrigo.adomes@gmail.com>
2026-08-17 21:59:28 -0300

    feat(tenants): move Salvatex storefront to salvatex.radzhub.com.br

--- functions/salvatex/index.js
+++ functions/salvatex/index.js
@@ -1,32 +1,6 @@
-class HeadHandler {
-  element(element) {
-    element.prepend('<base href="/">', { html: true });
-    element.append('<link rel="canonical" href="https://radzhub.com.br/salvatex/">', { html: true });
-  }
-}
-
-class SalvatexLinkHandler {
-  element(element) {
-    const href = element.getAttribute('href') || '';
-    if (href === 'index.html' || href === './' || href === '/') element.setAttribute('href', '/salvatex/');
-  }
-}
-
 export async function onRequest(context) {
   const url = new URL(context.request.url);
-  const assetUrl = new URL('/index.html', url.origin);
-  const response = await context.env.ASSETS.fetch(new Request(assetUrl.toString(), context.request));
-  const headers = new Headers(response.headers);
-  headers.set('Cache-Control', 'no-cache');
-  headers.delete('content-length');
-  const htmlResponse = new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers,
-  });
-
-  return new HTMLRewriter()
-    .on('head', new HeadHandler())
-    .on('a[href]', new SalvatexLinkHandler())
-    .transform(htmlResponse);
+  const target = new URL('https://salvatex.radzhub.com.br/');
+  target.search = url.search;
+  return Response.redirect(target.toString(), 301);
 }
